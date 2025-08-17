866a55651daa84637296adb3de3da822a7754123
Victor Ulloa <victoralfonsoulloa@gmail.com>
2025-08-17 11:12:19 -0600

    Add comprehensive comments to JavaScript and media queries for better code documentation

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+/**
+ * Portfolio Website JavaScript
+ * Author: Victor Ulloa
+ * Description: Handles mobile navigation toggle functionality
+ */
+
+/**
+ * Toggle the mobile hamburger menu
+ * Adds/removes 'open' class to show/hide menu and animate hamburger icon
+ */
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    
+    // Toggle the 'open' class on both menu and icon for animation
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
